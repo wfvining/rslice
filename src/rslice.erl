9f7de5c43d0c195efa367caecdd861530d2b1ad5
Will Vining <wfvining@gmail.com>
2020-05-21 23:35:32 -0600

    Basic API implementation
    - create a new slice table
    - join nodes to the slice table
    - lookup the node responsible for a given key

--- src/rslice.erl
+++ src/rslice.erl
@@ -1,8 +1,60 @@
 -module(rslice).
+-export([new/0, join/2, owner/2]).
+-export_type([table/0]).
 
--export([empty/0]).
+-type node_id() :: any().
+-type key() :: iodata().
+-type capacity() :: pos_integer().
 
-%% @doc Create an empty random slicing table.
--spec empty() -> inteval_table:table().
-empty() ->
-    inteval_table:empty().
+-record(slice, {table = interval_table:new() :: interval_table:table(),
+                nodes = #{} :: #{node_id() => capacity()}}).
+
+-opaque table() :: #slice{}.
+
+%% 2^128 - 1
+-define(MAX_HASH, 340282366920938463463374607431768211455).
+
+%% @doc Create a new (empty) random slicing table.
+-spec new() -> table().
+new() ->
+    #slice{}.
+
+%% @doc Join keys in `Keys' to the slice table.
+-spec join(#{node_id() => capacity()}, table()) -> table().
+join(NewKeys, Table=#slice{nodes=Nodes, table=IntervalTable}) ->
+    CurrentTotalCapacity = sum_values(Nodes),
+    NewTotalCapacity = CurrentTotalCapacity + sum_values(NewKeys),
+    CapacityChanges =
+        maps:map(
+          fun(_, Capacity) ->
+                  rational:subtract(
+                    rational:new(Capacity, CurrentTotalCapacity),
+                    rational:new(Capacity, NewTotalCapacity))
+          end, Nodes),
+    NewRelativeCapacity =
+        maps:map(fun(_, Capacity) ->
+                         rational:new(Capacity, NewTotalCapacity)
+                 end, NewKeys),
+    Table#slice{
+      nodes = maps:merge(Nodes, NewKeys),
+      table =
+          interval_table:assign(
+            NewRelativeCapacity,
+            interval_table:shrink(CapacityChanges, IntervalTable))}.
+
+sum_values(Map) ->
+    maps:fold(fun(_, X, Acc) ->
+                      X + Acc
+              end, 0, Map).
+
+-spec owner(key(), table()) -> node_id().
+owner(Key, #slice{table = IntervalTable}) ->
+    KeyHash = hash(Key),
+    {ok, Node} = interval_table:lookup(KeyHash, IntervalTable),
+    Node.
+
+-spec hash(key()) -> rational:rational().
+hash(Key) ->
+    HashState = crypto:hash_update(crypto:hash_init(blake2b), Key),
+    <<Hash:128, _/binary>> = crypto:hash_final(HashState),
+    rational:new(Hash, ?MAX_HASH).
--- test/rslice_test.erl
+++ test/rslice_test.erl
@@ -1,2 +1,36 @@
 -module(rslice_test).
 -include_lib("eunit/include/eunit.hrl").
+
+join_nodes_test_() ->
+    {"Can join nodes to the slice table",
+     {setup,
+      fun rslice:new/0,
+      fun(SliceTable) ->
+              [?_test(rslice:join(#{foo => 100}, SliceTable)),
+               ?_test(rslice:join(#{foo => 100, bar => 100}, SliceTable)),
+               ?_test(rslice:join(#{foo => 10, bar => 100}, SliceTable)),
+               ?_test(rslice:join(#{}, SliceTable)),
+               ?_test(
+                  rslice:join(
+                    #{baz => 200},
+                    rslice:join(#{foo => 100, bar => 100}, SliceTable)))]
+      end}}.
+
+lookup_key_test_() ->
+    {"Can look up the responsible node for a key",
+     {setup,
+      fun() -> rslice:join(#{foo => 100}, rslice:new()) end,
+      fun(SliceTable) ->
+              [?_assertEqual(foo, rslice:owner("test", SliceTable)),
+               {with, SliceTable, [fun lookup_two_keys/1]}]
+      end}}.
+
+lookup_two_keys(SliceTable) ->
+    Table = rslice:join(#{bar => 200}, SliceTable),
+    Owners = [rslice:owner(
+                io_lib:format("key-~3..0B", [Key]), Table) ||
+                 Key <- lists:seq(1, 100)],
+    [?_assert(lists:member(foo, Owners)),
+     ?_assert(lists:member(bar, Owners)),
+     ?_assert(math:abs(50 - length(Owners -- [foo])) < 5),
+     ?_assert(math:abs(50 - length(Owners -- [bar])) < 5)].
