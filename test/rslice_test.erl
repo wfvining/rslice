11a595c92839168a38103913deebe43075ced8ce
Will Vining <wfvining@gmail.com>
2020-05-23 19:53:36 -0600

    stubs for tests of multiple owners functionality.
    Not completely sure of the behavior I want for requesting more owners
    than are available... throwing an exception is probably right, but I
    want to give it a little more thought.

--- test/rslice_test.erl
+++ test/rslice_test.erl
@@ -34,3 +34,39 @@ lookup_two_keys(SliceTable) ->
      ?_assert(lists:member(bar, Owners)),
      ?_assert(math:abs(50 - length(Owners -- [foo])) < 5),
      ?_assert(math:abs(50 - length(Owners -- [bar])) < 5)].
+
+multiple_owners_test_() ->
+    {setup,
+     fun twenty_nodes/0,
+     fun(SliceTable) ->
+             [{"correct number of owners are returned",
+               fun() ->
+                       [?_assertEqual(
+                           1,
+                           length(rslice:owner("foo", 1, SliceTable))),
+                        ?_assertEqual(
+                           12,
+                           length(rslice:owner("foo", 12, SliceTable)))]
+               end},
+              {"crash when more owners are requested than there "
+               "are nodes in the slice table",
+               fun() -> ?assert(false) end},
+              {"all returned owners are unique",
+               fun() -> ?assert(false) end},
+              {"all nodes are returned when the number of owners "
+               "requested is same as number of nodes",
+               fun() -> ?assert(false) end},
+              {"an empty list is returned when zero owners are requested",
+               fun() -> ?assert(false) end},
+              {"decreasing the number of requested owners returns "
+               "a prefix of the longer list",
+               fun() -> ?assert(false) end}
+             ]
+     end}.
+
+twenty_nodes() ->
+    rslice:join(
+      maps:from_list(
+        [{integer_to_list(X), 100} || X <- lists:seq(0, 10)] ++
+        [{integer_to_list(X), 200} || X <- lists:seq(10, 20)]),
+      rslice:new()).
